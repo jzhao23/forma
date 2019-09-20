cf718b1120508ed3482d9caa005b90bf1d138216
jacobwagner8 <jtwagner@stanford.edu>
2019-09-20 12:38:06 -0700

    Adding stuff for sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,19 +2,40 @@ import React from 'react';
 import {Droppable} from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import Class from './Class';
+import Quarter from './Quarter';
+import requirementsData from './requirementsData'
 
 const Title = styled.h3`
   padding: 8px;
   padding-bottom: 2px;
   margin: 2px;
   border: 10px solid green;
+  max-width: 150px;
+`;
+
+const Container = styled.div`
+  margin-top: 10px;
+  border: 1px solid green;
+  border-radius: 2px;
+  max-width: 150px;
+  word-wrap: break-word;
 `;
 
 class Sidebar extends React.Component{
+  
+  constructor(){
+    super();
+    this.requirements = requirementsData;
+  };
+
   render() {
     return (
       <Title>
-      <input type="text" className="input" placeholder="Search..." />
+        <input type="text" className="input" placeholder="Search..." />
+        <Container>
+          const majorID = "CS";
+          const major = this.state.Majors[majorID];
+        </Container>
       </Title>
     );
   }
---
+++ src/requirementsData.js
@@ -0,0 +1,76 @@
+const requirementsData = {
+  Majors: {
+    'CS': {
+      id: 'CS',
+      name: 'Introduction to Computer Science',
+    },
+    'Math': {
+      id: 'Math',
+      name: 'Computer Systems',
+    },
+    'Philosophy': {
+      id: 'Philosophy',
+      name: 'Computer Vision',
+    },
+    'Psychology': {
+      id: 'Psychology',
+      name: 'Introduction to Philosophy',
+    },
+    'Economics': {
+      id: 'ECON1',
+      name: 'Introduction to Economics',
+    },
+  },
+  quarters: {
+    'Autumn 19': {
+      id: 'Autumn 19',
+      classes: ['CS106A', 'ECON1'],
+    },
+    'Winter 20': {
+      id: 'Winter 20',
+      classes: ['CS107', 'PHIL1'],
+    },
+    'Spring 20': {
+      id: 'Spring 20',
+      classes: ['CS231N'],
+    },
+    'Autumn 20': {
+      id: 'Autumn 20',
+      classes: [],
+    },
+    'Winter 21': {
+      id: 'Winter 21',
+      classes: [],
+    },
+    'Spring 21': {
+      id: 'Spring 21',
+      classes: [],
+    },
+    'Autumn 21': {
+      id: 'Autumn 21',
+      classes: [],
+    },
+    'Winter 22': {
+      id: 'Winter 22',
+      classes: [],
+    },
+    'Spring 22': {
+      id: 'Spring 22',
+      classes: [],
+    },
+    'Autumn 22': {
+      id: 'Autumn 22',
+      classes: [],
+    },
+    'Winter 23': {
+      id: 'Winter 23',
+      classes: [],
+    },
+    'Spring 23': {
+      id: 'Spring 23',
+      classes: [],
+    },
+  },
+}
+
+export default requirementsData;
